ad8bdcb2e5091c2bb0358dac4cc4d55f941cb56b
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-01-31 11:08:56 -0300

    heroku add build app

--- dist/shared/infra/http/server.js
+++ dist/shared/infra/http/server.js
@@ -47,5 +47,5 @@ app.use((err, request, response, _) => {
   });
 });
 app.listen(process.env.PORT || 3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('🚀 Server started on port ', process.env.PORT);
 });
