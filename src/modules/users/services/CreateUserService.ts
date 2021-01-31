1f3984354164f8f149701c28cfeead858cd904a9
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-01-31 18:09:23 -0300

    remove cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,7 +2,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 
 import { inject, injectable } from 'tsyringe';
-import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+// import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IHashProvider from '../providers/HashProvider/models/IHashProviders';
 
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -22,9 +22,9 @@ class CreateUserService {
     @inject('HashProvider')
     private hashProvider: IHashProvider,
 
-    @inject('CacheProvider')
-    private cacheProvider: ICacheProvider,
-  ) {}
+    // @inject('CacheProvider')
+    // private cacheProvider: ICacheProvider,
+  ) { }
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const checkUserExists = await this.usersRepository.findByEmail(email);
@@ -41,7 +41,7 @@ class CreateUserService {
       password: hashedPassword,
     });
 
-    await this.cacheProvider.invalidatePrefix('providers-list');
+    // await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
