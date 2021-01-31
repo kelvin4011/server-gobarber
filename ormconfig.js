e427ac8a2f83f1e159eee15b3921084e47d79edd
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-01-31 12:25:30 -0300

    adjusting ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -3,10 +3,10 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": [
-    ".dist/src/modules/**/infra/typeorm/entities/*.js"
+    "./dist/modules/**/infra/typeorm/entities/*.js"
   ],
   "migrations": [
-    ".dist/src/shared/infra/typeorm/migrations/*.js"
+    "./dist/shared/infra/typeorm/migrations/*.js"
   ],
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
