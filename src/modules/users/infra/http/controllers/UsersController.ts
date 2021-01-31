ad5da46ea5b13fcd41bd083e72c7304b1c354ec2
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-01-31 14:40:11 -0300

    adjusting ormconfig pt12

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -8,6 +8,7 @@ export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body;
 
+    console.log({ name, email, password });
     const createUser = container.resolve(CreateUserService);
 
     const user = await createUser.execute({
@@ -15,7 +16,6 @@ export default class UsersController {
       email,
       password,
     });
-    console.log(user);
 
     return response.json(classToClass(user));
   }
