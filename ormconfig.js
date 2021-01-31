ed7106efa98ec74387baf483f18c49beaf7d6341
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-01-31 11:43:51 -0300

    config ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-console.log(process.env.DATABASE_URL);
+console.log('vc esta usando a url: ', process.env.DATABASE_URL);
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
